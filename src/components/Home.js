c7d0f3d4db11dfff027277354c671a2ee89c81fb
Rudin Swagerman <post@rudinswagerman.nl>
2020-05-22 10:12:34 +0200

    setting up the 'home page'

---
+++ src/components/Home.js
@@ -0,0 +1,60 @@
+import { h } from "preact"
+
+const Home = () => (
+  <>
+    <section>
+      <p>
+        {`Sounds Like Touch aims to develop and encourage a for both performer and audience tangible and accessible practice of creating and performing electronic and electroacoustic music.`}
+      </p>
+      <p>
+        {`Sounds Like Touch aims to develop and encourage a for both performer and audience tangible and accessible practice of creating and performing electronic and electroacoustic music.`}
+      </p>
+      <p>
+        {`Sounds Like Touch enables the evolution of new methods, means, technologies and products for making electronic-and electroacoustic music.`}
+      </p>
+      <p>
+        {`Sounds Like Touch is a platform and springboard for artists, makers and musicians.`}
+      </p>
+    </section>
+    <section>
+      <p>
+        <h1>Who</h1>
+        Dianne Verdonk Initiator
+      </p>
+      <p>
+        {`Performer, composer and instrument developer - who seeks the ultimate, personal form of musical expression in the creation and performance of electronic/electroacoustic music.`}
+      </p>
+      <p>
+        Rens Machielse
+        <br />
+        Board member
+      </p>
+      <p>
+        Rob van Rijswijk
+        <br />
+        Board member
+      </p>
+      <p>
+        Marisa Tempel
+        <br />
+        Board member
+      </p>
+    </section>
+    <section>
+      <p>
+        <h1>Contact</h1>
+        Sounds Like Touch
+        <br />
+        Europalaan 2B - F1.19
+        <br />
+        Utrecht, NL
+        <br />
+        diannedsoundsliketouch.eu
+        <br />
+        CoC: 74526707
+      </p>
+    </section>
+  </>
+)
+
+export default Home
