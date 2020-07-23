56ff2496664e5a78bae1257d9f5fd9fb85db6ddc
Rudin Swagerman <post@rudinswagerman.nl>
2020-07-23 23:16:27 +0200

    try this, resume on touchstart

--- src/components/app.js
+++ src/components/app.js
@@ -74,7 +74,7 @@ const App = () => {
 
   useEffect(() => {
     window.document.body.addEventListener(
-      "touchend",
+      "touchstart",
       () => contextRef.current.resume(),
       false
     )
