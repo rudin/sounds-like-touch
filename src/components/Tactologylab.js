ac5658372aebe705e271af144e33a5c55cfbbbab
dianneverdonk <dianneverdonk@gmail.com>
2022-02-21 14:53:25 +0100

    Update Tactologylab.js

--- src/components/Tactologylab.js
+++ src/components/Tactologylab.js
@@ -64,8 +64,8 @@ const Tactologylab = () => {
             <li><i>Day 2: prototype </i> &nbsp;<b>19 March</b> &nbsp;&nbsp; morning + afternoon</li>
             <li><i>Day 3: construct </i> &nbsp;<b>20 March</b> &nbsp;&nbsp; morning + afternoon</li>
             <li><i>Day 4: express </i> &nbsp;&nbsp; <b>27 March</b>&nbsp; &nbsp; morning + afternoon</li>
-            <li><i>Day 5: embed </i> &nbsp;&nbsp; &nbsp;&nbsp; <b>1 April*</b>&nbsp; &nbsp; morning + afternoon</li>
-            <li><i>Day 6: perform </i> &nbsp;<b>9 April</b>&nbsp; &nbsp; one of these days</li>
+            <li><i>Day 5: embed </i> &nbsp;&nbsp; &nbsp;&nbsp; <b>1 April</b>&nbsp; &nbsp; morning + afternoon</li>
+            <li><i>Day 6: perform </i> &nbsp;&nbsp; <b>9 April</b>&nbsp; &nbsp; one of these days</li>
           </ul>
         </p>
 
