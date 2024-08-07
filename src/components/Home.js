bcf5dc06be1b8dfa04c59ed97db9f59a6521e2c6
dianneverdonk <dianneverdonk@gmail.com>
2024-08-07 18:12:34 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -74,7 +74,7 @@ const Home = () => {
           <i>Dianne Verdonk</i>
         </a>
          : artistic director & founder 
-
+<p>
           {`also: performer, songwriter and instrument inventor`}
         <p>
           <a
